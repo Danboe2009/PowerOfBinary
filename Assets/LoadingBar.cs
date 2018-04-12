50f278d0879a46030c4df230fb9ee8146fdecdc5
Danboe2009 <Danboe2009@gmail.com>
2018-04-12 11:45:23 -0400

    Started working on the game.

---
+++ Assets/LoadingBar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingBar : MonoBehaviour {
+
+	public Renderer renderer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () { 
+		renderer.material.SetFloat("_Cutoff", Mathf.InverseLerp(0, Screen.width, Input.mousePosition.x)); 
+	}
+}
